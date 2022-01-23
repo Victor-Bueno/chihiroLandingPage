183a2cb5016a56c6518219fc066fbe107ee624e1
Victor Bueno <victor.bueno0720@gmail.com>
2022-01-23 10:43:37 -0300

    Removed unnecessary files and adjusted project structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+function Home() {
+  return (
+    <div className="page-container">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Home;
